a58abde10422a7dd972ce992b0e199d8cf83eb54
Sumon Ahmed <bdsumon4u@gmail.com>
2022-03-08 09:44:52 +0600

    DashboardReport EachStatus

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -24,12 +24,12 @@ class HomeController extends Controller
 
         $productsCount = Product::count();
         $orderQ = Order::query()->whereBetween('created_at', [$_start->startOfDay()->toDateTimeString(), $_end->endOfDay()->toDateTimeString()]);
-        $ordersCount = with($orderQ)->count();
-        $initialStatus = data_get(config('app.orders'), 0, 'PENDING');
-        $initialOrdersCount = with($orderQ)->whereStatus($initialStatus)->count();
-        $returnedOrdersCount = with($orderQ)->whereStatus('returned')->count();
+        $orders = ['Total' => (clone $orderQ)->count()];
+        foreach (config('app.orders', []) as $status) {
+            $orders[$status] = (clone $orderQ)->where('status', $status)->count();
+        }
         $inactiveProducts = Product::whereIsActive(0)->get();
         $outOfStockProducts = Product::whereShouldTrack(1)->where('stock_count', '<=', 0)->get();
-        return view('admin.dashboard', compact('productsCount', 'ordersCount', 'initialStatus', 'initialOrdersCount', 'returnedOrdersCount', 'inactiveProducts', 'outOfStockProducts', 'start', 'end'));
+        return view('admin.dashboard', compact('productsCount', 'orders', 'inactiveProducts', 'outOfStockProducts', 'start', 'end'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -32,6 +32,15 @@
    <div class="row size-column">
       <div class="col-xl-7 box-col-12 xl-100">
          <div class="row dash-chart">
+             <div class="col-12">
+                 <div class="mb-3">
+                     @foreach(config('app.orders', []) as $status)
+                         <a href="{{ route('admin.orders.index', ['status' => strtolower($status)]) }}" class="btn btn-primary btn-light text-dark px-2 py-1 mx-2 my-1">
+                             <span>{{ $status }}</span>
+                         </a>
+                     @endforeach
+                 </div>
+             </div>
             <div class="col-xl-4 box-col-4 col-lg-4 col-md-4">
                <div class="card o-hidden">
                   <div class="card-body">
@@ -71,48 +80,20 @@
                   </div>
                </div>
             </div>
-            <div class="col-xl-4 box-col-4 col-lg-4 col-md-4">
+             @foreach($orders as $status => $count)
+            <div class="col-xl-3 box-col- col-lg-3 col-md-3">
                <div class="card o-hidden">
                   <div class="card-body">
                      <div class="media">
                         <div class="media-body">
-                           <p class="f-w-500 font-roboto">TOTAL Orders</p>
-                           <h4 class="f-w-500 mb-0 f-26"><span class="counter">{{ $ordersCount }}</span></h4>
+                           <p class="f-w-500 font-roboto">{{ $status }} Orders</p>
+                           <h4 class="f-w-500 mb-0 f-26"><span class="counter">{{ $count }}</span></h4>
                         </div>
                      </div>
                   </div>
                </div>
             </div>
-            <div class="col-xl-4 box-col-4 col-lg-4 col-md-4">
-               <div class="card o-hidden">
-                  <div class="card-body">
-                     <div class="media">
-                        <div class="media-body">
-                           <p class="f-w-500 font-roboto">{{ $initialStatus }} Orders</p>
-                           <h4 class="f-w-500 mb-0 f-26"><span class="counter">{{ $initialOrdersCount }}</span></h4>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-            <div class="col-xl-4 box-col-4 col-lg-4 col-md-4">
-               <div class="card o-hidden">
-                  <div class="card-body">
-                     <div class="media">
-                        <div class="media-body">
-                           <p class="f-w-500 font-roboto">RETURN Rate</p>
-                           <div class="progress-box">
-                              <h4 class="f-w-500 mb-0 f-26"><span class="counter">{{ $returnedOrdersCount }}</span></h4>
-                              <div class="progress sm-progress-bar progress-animate app-right d-flex justify-content-end">
-                                 @exp($percentage = $ordersCount ? round($returnedOrdersCount * 100 / $ordersCount) : 0)
-                                 <div class="progress-gradient-primary" role="progressbar" style="width: {{$percentage}}%" aria-valuenow="{{$percentage}}" aria-valuemin="0" aria-valuemax="100"><span class="font-primary">{{$percentage}} %</span><span class="animate-circle"></span></div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
+             @endforeach
          </div>
       </div>
       <div class="col-xl-4 xl-50 box-col-12">
@@ -156,44 +137,42 @@
                </div>
             </div>
          </div>
-      </div>
-      <div class="col-xl-4 xl-50 box-col-12">
-         <div class="card">
-            <div class="card-header p-4 card-no-border">
-               <h5>Out Of Stock</h5>
-            </div>
-            <div class="card-body p-3">
-               <div class="our-product">
-                  <div class="table-responsive">
-                     <table class="table table-bordered">
-                        <tbody class="f-w-500">
-                           @foreach($outOfStockProducts as $product)
-                           <tr>
-                              <td class="pl-2">
-                                 <div class="media">
-                                    <img class="img-fluid m-r-15 rounded-circle" src="{{ optional($product->base_image)->src }}" width="42" height="42" alt="">
-                                    <div class="media-body">
-                                       <a href="{{ route('admin.products.edit', $product) }}">{{ $product->name }}</a>
-                                    </div>
-                                 </div>
-                              </td>
-                              <td>
-                                 <p>SKU</p>
-                                 <span>{{ $product->sku }}</span>
-                              </td>
-                              <td class="text-center">
-                                 <del style="color: #ff0000;">{!!  theMoney($product->price)  !!}</del>
-                                 <br>
-                                 <ins style="text-decoration: none;">{!!  theMoney($product->selling_price)  !!}</ins>
-                              </td>
-                           </tr>
-                           @endforeach
-                        </tbody>
-                     </table>
+          <div class="card">
+              <div class="card-header p-4 card-no-border">
+                  <h5>Out Of Stock</h5>
+              </div>
+              <div class="card-body p-3">
+                  <div class="our-product">
+                      <div class="table-responsive">
+                          <table class="table table-bordered">
+                              <tbody class="f-w-500">
+                              @foreach($outOfStockProducts as $product)
+                                  <tr>
+                                      <td class="pl-2">
+                                          <div class="media">
+                                              <img class="img-fluid m-r-15 rounded-circle" src="{{ optional($product->base_image)->src }}" width="42" height="42" alt="">
+                                              <div class="media-body">
+                                                  <a href="{{ route('admin.products.edit', $product) }}">{{ $product->name }}</a>
+                                              </div>
+                                          </div>
+                                      </td>
+                                      <td>
+                                          <p>SKU</p>
+                                          <span>{{ $product->sku }}</span>
+                                      </td>
+                                      <td class="text-center">
+                                          <del style="color: #ff0000;">{!!  theMoney($product->price)  !!}</del>
+                                          <br>
+                                          <ins style="text-decoration: none;">{!!  theMoney($product->selling_price)  !!}</ins>
+                                      </td>
+                                  </tr>
+                              @endforeach
+                              </tbody>
+                          </table>
+                      </div>
                   </div>
-               </div>
-            </div>
-         </div>
+              </div>
+          </div>
       </div>
    </div>
 </div>
